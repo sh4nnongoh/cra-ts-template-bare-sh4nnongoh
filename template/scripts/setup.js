d7b5d7e8f42d17cd20f4d1715f5f1824541170c7
Shannon <sh4nnongoh@gmail.com>
2022-06-11 14:52:35 +0800

    Add Husky

--- template/scripts/setup.js
+++ template/scripts/setup.js
@@ -1,23 +1,25 @@
 // Update the default package.json
 const { execSync } = require("child_process");
 const fs = require("fs");
-const data = fs.readFileSync("package.json", {encoding:"utf8"});
+const data = fs.readFileSync("package.json", { encoding: "utf8" });
 const objects = JSON.parse(data);
 const updatedObjects = {
   ...objects,
-  "scripts": {
+  scripts: {
     ...objects.scripts,
-    "test": "react-scripts test --coverage --watchAll",
-    "lint": "yarn eslint --ext .ts,.tsx src/ --fix",
+    test: "react-scripts test --coverage --watchAll",
+    "test:ci": "cross-env CI=true react-scripts test",
+    lint: "yarn eslint --ext .ts,.tsx src/ --fix",
+    prepare: "husky install"
   },
-  "jest": {
-    "collectCoverageFrom": ["./src/**"],
-    "coverageThreshold": {
-      "global": {
-        "lines": 90
+  jest: {
+    collectCoverageFrom: ["./src/**"],
+    coverageThreshold: {
+      global: {
+        lines: 90
       }
     },
-    "coveragePathIgnorePatterns": [
+    coveragePathIgnorePatterns: [
       "reportWebVitals.ts",
       "react-app-env.d.ts",
       "index.tsx"
@@ -27,3 +29,11 @@ const updatedObjects = {
 console.log(updatedObjects);
 fs.writeFileSync("package.json", JSON.stringify(updatedObjects, null, 2));
 execSync("yarn");
+execSync("rm -rf .husky");
+execSync("yarn prepare");
+execSync("yarn husky add .husky/pre-commit \"yarn lint\"");
+execSync("yarn husky add .husky/pre-push \"yarn test:ci\"");
+execSync("git reset");
+execSync("git add .husky/pre-commit");
+execSync("git add .husky/pre-push");
+execSync("git commit -m '[Config] Enable Husky'");
